b651e67486f3ed6a2c4b263b06c2d22208473a15
moinul <1405109.mmh@ugrad.cse.buet.ac.bd>
2022-04-13 01:43:40 +0600

    code clean up

--- src/main/java/com/kafka/KafkaApplication.java
+++ src/main/java/com/kafka/KafkaApplication.java
@@ -5,10 +5,6 @@ import org.springframework.boot.*;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.Bean;
 import org.springframework.kafka.core.KafkaTemplate;
-import org.springframework.kafka.support.KafkaHeaders;
-import org.springframework.messaging.support.GenericMessage;
-
-import java.util.Collections;
 
 @SpringBootApplication
 public class KafkaApplication {
